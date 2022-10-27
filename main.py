221b731a4fe817248c45c685cab16a3713c38713
NMcSheehy <nmflash8@byui.edu>
2022-10-27 14:41:07 -0600

    commit

---
+++ main.py
@@ -0,0 +1,34 @@
+# import time
+# time.sleep(.25)
+
+# Director
+# manage the game (keep the game playing)
+# keeps track of score
+# Keeps the game moving (uses time)
+
+# Field
+# Need to have an area to play the game
+# have borders
+# falling physics
+
+# Objects
+# control gravity
+
+    # Player
+        # controls 
+        # allow you to move with arrow keys rather than typing 'left' or 'right'
+
+
+    # Rocks
+    # will subtract points if they touch player
+    # removes object after collision
+
+    # Gems
+    # will add points if they touch player
+    # removes object after collision
+
+# Spawn
+# randomly generate rocks and gems at the top of the screen
+
+# Collision
+# 
