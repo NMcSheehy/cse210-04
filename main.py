0277bb9186c46b348f76118aa0005c895596fff2
NMcSheehy <nmflash8@byui.edu>
2022-10-27 14:45:29 -0600

    who's doing what

--- main.py
+++ main.py
@@ -7,28 +7,35 @@
 # Keeps the game moving (uses time)
 
 # Field
+# Noah
 # Need to have an area to play the game
 # have borders
 # falling physics
 
 # Objects
+# Paul
 # control gravity
 
     # Player
+    # Noah
         # controls 
         # allow you to move with arrow keys rather than typing 'left' or 'right'
 
 
     # Rocks
+    # Edward
     # will subtract points if they touch player
     # removes object after collision
 
     # Gems
+    # Edward
     # will add points if they touch player
     # removes object after collision
 
 # Spawn
+# David
 # randomly generate rocks and gems at the top of the screen
 
 # Collision
+# Paul
 # 
