05193baef1084c6e1f7bc6cd31b37d4702a62332
NMcSheehy <nmflash8@byui.edu>
2022-11-04 16:55:04 -0600

    commit

--- example.py
+++ example.py
@@ -1,137 +1,71 @@
+# import pygame module in this program
 import pygame
 
-def main():
+# activate the pygame library
+# initiate pygame and give permission
+# to use pygame's functionality.
+pygame.init()
 
-    # Define Variables
-    width = 900
-    height = 500
-    name = 'cse210-04'
-    background = (255, 255, 255)
-    FPS = 60
-    score = 0
+# define the RGB value for white,
+# green, blue colour .
+white = (255, 255, 255)
+green = (0, 255, 0)
+blue = (0, 0, 128)
 
-    # Player Variables
-    player_path = 'player.png'
-    player_height = 100
-    player_width = 100
-    player_x = 450
-    player_y = 400
-    speed = 10
+# assigning values to X and Y variable
+X = 400
+Y = 400
 
-    # Rock Variables
-    rock_coords = [(40,50)]
-    rock_path = 'rock.png'
-    rock_height = 100
-    rock_width = 100
-    rock_speed = 5
+# create the display surface object
+# of specific dimension..e(X, Y).
+display_surface = pygame.display.set_mode((X, Y))
 
-    # Gem Variables
+# set the pygame window name
+pygame.display.set_caption('Show Text')
 
+# create a font object.
+# 1st parameter is the font file
+# which is present in pygame.
+# 2nd parameter is size of the font
+font = pygame.font.Font('freesansbold.ttf', 32)
 
-    # Load in the player
-    player_image = pygame.image.load(player_path)
-    player = pygame.transform.scale(player_image, (player_width, player_height))
-    player_hitbox = pygame.Rect(player_x, player_y, player_width, player_height)
+# create a text surface object,
+# on which text is drawn on it.
+text = font.render('GeeksForGeeks', True, green, blue)
 
-    # Load in Rock
-    rock_image = pygame.image.load(rock_path)
-    rock = pygame.transform.scale(rock_image, (rock_width, rock_height))
+# create a rectangular object for the
+# text surface object
+textRect = text.get_rect()
 
-    # Load in Gem
+# set the center of the rectangular object.
+textRect.center = (X // 2, Y // 2)
 
-    # Define Game Window
-    WIN = pygame.display.set_mode((width, height))
-    pygame.display.set_caption(name)
+# infinite loop
+while True:
 
-    # Setup the game clock (FPS)
-    clock = pygame.time.Clock()
+	# completely fill the surface object
+	# with white color
+	display_surface.fill(white)
 
-    # Start the game loop
-    run = True
-    while run:
+	# copying the text surface object
+	# to the display surface object
+	# at the center coordinate.
+	display_surface.blit(text, textRect)
 
-        # Make sure Frame Rate doesn't go over FPS
-        clock.tick(FPS)
+	# iterate over the list of Event objects
+	# that was returned by pygame.event.get() method.
+	for event in pygame.event.get():
 
-        # Keep track of the in game events
-        for event in pygame.event.get():
+		# if event object type is QUIT
+		# then quitting the pygame
+		# and program both.
+		if event.type == pygame.QUIT:
 
-            # Event: Quit game
-            if event.type == pygame.QUIT:
-                run = False
+			# deactivates the pygame library
+			pygame.quit()
 
-        # Find all keys pressed
-        keys_pressed = pygame.key.get_pressed()
+			# quit the program.
+			quit()
 
-        # Move player left
-        if keys_pressed[pygame.K_LEFT]:
-            player_hitbox.x -= speed
-
-        # Move player right
-        if keys_pressed[pygame.K_RIGHT]:
-            player_hitbox.x += speed
-
-        # Update window
-        draw_window(WIN, background, player, player_hitbox)
-    pygame.quit()
-
-def draw_window(WIN, background, player, player_hitbox):
-    # This has to be in order from bottom layer to top layer. 
-    # Other wise the back ground will just draw over the scene
-
-    # Background Color
-    WIN.fill(background)
-
-    # Draw player
-    WIN.blit(player, (player_hitbox.x, player_hitbox.y))
-
-    # Change the screen
-    pygame.display.update()
-
-
-class Objects():
-    def __init__(self, path):
-        self.path = path
-
-
-
-
-
-
-
-if __name__ == '__main__':
-    main()
-
-# Field
-# Noah
-# Need to have an area to play the game
-# have borders
-# falling physics
-
-# Objects
-# Paul
-# control gravity
-
-    # Player
-    # Noah
-        # controls 
-        # allow you to move with arrow keys rather than typing 'left' or 'right'
-
-
-    # Rocks
-    # Edward
-    # will subtract points if they touch player
-    # removes object after collision
-
-    # Gems
-    # Edward
-    # will add points if they touch player
-    # removes object after collision
-
-# Spawn
-# David
-# randomly generate rocks and gems at the top of the screen
-
-# Collision
-# Paul
-# 
+		# Draws the surface object to the screen.
+		pygame.display.update()
--- main.py
+++ main.py
@@ -1,113 +1,218 @@
 import pygame
+import random
+pygame.init()
+
+def main():
+    # load Game
+    game = Director()
+
+    # Load Window
+    window = Window(900, 500, 'cse210-04 DINO ROCKET!!!', (255, 255, 255), 60)
+
+    # Load Speech
+    speech = Speech()
+
+    # load Points
+    points = Points(10)
+
+    # Load in Player
+    player = Player('player.png', 100, 100, 450, 400, 10)
+
+    # Load in Rock
+    rock = Rock('rock.png', 50, 50, random.randint(0,900), -150, 20)
+
+    # Load in Gem
+    gem = Gem('gem.png', 50, 50, random.randint(0,900), -150, 9)
+
+    game.start(player, rock, gem, window, points, speech)
 
 class Director():
-    # Manage the game / events
-    # keeps track of score
-    # Keeps the game moving
-    def __init__(self, width, height, name, background, FPS, score):
+    # Manages the game (movement / collision)
+    # holds the loop
+    # keeps the game playing
+    def __init__(self) -> None:
+        self.playing = True
+
+        # Setup the game clock (FPS)
+        self.clock = pygame.time.Clock()
+
+    def start(self, player, rock, gem, window, points, speech):
+        # holds game loop aslong as self.playing is True
+        while self.playing:
+            # Make sure Frame Rate doesn't go over FPS
+            self.clock.tick(window.FPS)
+
+            # Keep track of the in game events
+            for event in pygame.event.get():
+
+                # Event: Quit game
+                if event.type == pygame.QUIT:
+                    self.playing = False
+
+                if points.points <= 0:
+                    self.playing = False
+
+            # check collision
+            gem.collision(player, points, speech)
+            rock.collision(player, points, speech)
+
+            # move player
+            player.move()
+
+            # move rock
+            rock.gravity()
+            rock.respawn()
+
+            # move gem
+            gem.gravity()
+            gem.respawn()
+
+            # Update window
+            window.draw(player, rock, gem, points, speech)
+        pygame.quit()
+
+class Window():
+    # holds the display for the game
+    # updates the window every frame
+    def __init__(self, width, height, name, background, FPS) -> None:
         self.width = width
         self.height = height
         self.name = name
-        self.background = background
+        self.background = background 
         self.FPS = FPS
-        self.score = score
 
-    def name_window(self):
-        # Sets the game window name
+
+        self.display = pygame.display.set_mode((self.width, self.height))
         pygame.display.set_caption(self.name)
 
-class Object():
-    def __init__(self, path):
-        self.path = pygame.image.load(path)
+    def draw(self, player, rock, gem, points, speech):
+        # This has to be in order from bottom layer to top layer. 
+        # Other wise the back ground will just draw over the scene
+        # Background Color
+        self.display.fill(self.background)
+
+        # Draw player
+        self.display.blit(player.image,(player.x, player.y))
+
+        # Draw rock
+        self.display.blit(rock.image, (rock.x, rock.y))
+
+        # Draw gem
+        self.display.blit(gem.image, (gem.x, gem.y))
+
+        # Update title to show points
+        pygame.display.set_caption(f'{self.name} | Score: {points.points}')
 
 
-class Player():
+        self.font = pygame.font.Font('freesansbold.ttf', 32)
+        text = self.font.render(F'{speech.message}', True, (0,0,0), (255, 255, 255))
+        textRect = text.get_rect()
+        self.display.blit(text, textRect)
+        
 
-    def __init__(self):
-        self.path = 'player.png'
+        # Change the screen
+        pygame.display.update()
 
-def main():
-    # Define Variables
-    # Game
-    width = 900
-    height = 500
-    name = 'cse210-04'
-    background = (255, 255, 255)
-    FPS = 60
-    score = 0
+class Objects():
+    # Class used for creating players, rocks and gems
+    def __init__(self, path, width, height, x, y, speed):
+        self.path = path
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        self.speed = speed
 
-    # Player
-    player_path = 'player.png'
+        # Load in image / hitbox
+        self.image_l = pygame.image.load(self.path)
+        self.image = pygame.transform.scale(self.image_l, (self.width, self.height))
+        self.hitbox = pygame.Rect(self.x, self.y, self.width, self.height)
 
-    # Define Classes
-    game = Director(width, height, name, background, FPS, score)
-    player = Object(player_path)
+class Rock(Objects):
+    # objects that will damage / remove points from the player.
+    def __init__(self, path, width, height, x, y, speed):
+        super().__init__(path, width, height, x, y, speed)
     
-    # Define Game window
-    WIN = pygame.display.set_mode((game.width, game.height))
-    game.name_window()
+    def gravity(self):
+        self.y += self.speed
+    
+    def respawn(self):
+        if self.y > 650:
+            self.y = -150
+            self.x = random.randint(0,900)
 
-    # Setup the game clock (FPS)
-    clock = pygame.time.Clock()
+    def collision(self, player, points, speech):
+        self.center_x = self.x + (self.width / 2)
+        self.center_y = self.y + (self.height / 2)
+        if self.center_x in range(player.x, player.x + player.width):
+            if self.center_y in range(player.y, player.y + player.height):
+                points.sub()
+                self.y = -150
+                self.x = random.randint(0,900)
+                speech.hurt()
 
-    # Start the game loop
-    run = True
-    while run:
+class Gem(Objects):
+    # Objects that will give the player points. (and a full stomach)
+    def __init__(self, path, width, height, x, y, speed):
+        super().__init__(path, width, height, x, y, speed)
 
-        # Make sure Frame Rate doesn't go over FPS
-        clock.tick(game.FPS)
+    def gravity(self):
+        self.y += self.speed
+    
+    def respawn(self):
+        if self.y > 650:
+            self.y = -150
+            self.x = random.randint(0,900)
+    
+    def collision(self, player, points, speech):
+        self.center_x = self.x + (self.width / 2)
+        self.center_y = self.y + (self.height / 2)
+        if self.center_x in range(player.x, player.x + player.width):
+            if self.center_y in range(player.y, player.y + player.height):
+                points.add()
+                self.y = -150
+                self.x = random.randint(0,900)
+                speech.eat()
 
-        # Keep track of the in game events
-        for event in pygame.event.get():
-            # Event: Quit game
-            if event.type == pygame.QUIT:
-                run = False
+class Player(Objects):
+    # object that moves around collecting points
+    def __init__(self, path, width, height, x, y, speed):
+        super().__init__(path, width, height, x, y, speed)
+    
+    def move(self):
+        # Find all keys pressed
+        keys_pressed = pygame.key.get_pressed()
 
-        draw_window(WIN, game.background, player.path)
-    pygame.quit()
+        # Move player left
+        if keys_pressed[pygame.K_LEFT]:
+            self.x -= self.speed
 
-def draw_window(WIN, background, player):
+        # Move player right
+        if keys_pressed[pygame.K_RIGHT]:
+            self.x += self.speed
 
-    WIN.blit(player, (300,100))
+class Points():
+    # score used to keep gaming playing
+    def __init__(self, points) -> None:
+        self.points = points
 
-    # Background Color
-    WIN.fill(background)
+    def add(self):
+        self.points += 2
 
-    pygame.display.update()
+    def sub(self):
+        self.points -= 6
+
+class Speech():
+    # Talks for the dinosour
+    def __init__(self) -> None:
+        self.message = 'Welcome!'
+
+    def eat(self):
+        self.message = 'Yum!'
+
+    def hurt(self):
+        self.message = 'Ouch...'
+
 
 if __name__ == '__main__':
     main()
-
-# Field
-# Noah
-# Need to have an area to play the game
-# have borders
-# falling physics
-
-# Objects
-# Paul
-# control gravity
-
-    # Player
-    # Noah
-        # controls 
-        # allow you to move with arrow keys rather than typing 'left' or 'right'
-
-
-    # Rocks
-    # Edward
-    # will subtract points if they touch player
-    # removes object after collision
-
-    # Gems
-    # Edward
-    # will add points if they touch player
-    # removes object after collision
-
-# Spawn
-# David
-# randomly generate rocks and gems at the top of the screen
-
-# Collision
-# Paul
-# 
